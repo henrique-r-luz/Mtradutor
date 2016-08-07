6bb0b84ce3b3d72212446ab477bff73a65d54ef1
henrique.luz <henrique.luz@ifnmg.edu.br>
2016-08-07 12:29:02 -0300

    add DAO

--- app/src/main/java/classes/mtradutor/CadastraPalavraActivity.java
+++ app/src/main/java/classes/mtradutor/CadastraPalavraActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import classes.mtradutor.modelo.DatabaseHelper;
+
 /**
  * Created by henrique on 29/02/16.
  */
@@ -12,9 +14,14 @@ public class CadastraPalavraActivity extends TamplateMtradutor {
 
 
 
+    private DatabaseHelper helper;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastra_palavra);
+
+        helper = new DatabaseHelper(this);
 
     }
 
